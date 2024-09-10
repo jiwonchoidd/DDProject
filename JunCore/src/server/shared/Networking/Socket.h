f73968f8d94c02f37452ca69b6d18a2d820dcf29
dkdldjswkd <dkdldjswkd@naver.com>
2024-09-11 00:17:09 +0900

    [최준영] common.lib 추가, shared.lib에 boost 라이브러리 링크

--- JunCore/src/server/shared/Networking/Socket.h
+++ JunCore/src/server/shared/Networking/Socket.h
@@ -1,10 +1,49 @@
- #include <boost/asio/ip/tcp.hpp>
+#include <Define.h>
+#include <vector>
+#include <queue>
+#include <atomic>
+#include <boost/asio/ip/tcp.hpp>
 
-template<class T, class Stream = boost::asio::ip::tcp::socket>
+using boost::asio::ip::tcp;
+
+struct MessageBuffer
+{
+	using storage	= std::vector<uint8>;
+	using size_type = std::vector<uint8>::size_type;
+
+public:
+	MessageBuffer() : _wpos(0), _rpos(0), _storage()
+	{
+		_storage.resize(4096);
+	}
+
+private:
+	size_type _wpos;
+	size_type _rpos;
+	storage _storage;
+};
+
+template<class T>
 class Socket : public std::enable_shared_from_this<T>
 {
 public:
-	int f() {
-		return 33;
+	explicit Socket(tcp::socket&& socket)
+		: _socket(std::move(socket)), _remoteAddress(_socket.remote_endpoint().address()), _remotePort(_socket.remote_endpoint().port()), _readBuffer(), _closed(false), _closing(false), _isWritingAsync(false)
+	{
+		_readBuffer.Resize(READ_BLOCK_SIZE);
 	}
+
+private:
+	tcp::socket _socket;
+
+	boost::asio::ip::address _remoteAddress;
+	uint16 _remotePort;
+
+	MessageBuffer _readBuffer;
+	std::queue<MessageBuffer> _writeQueue;
+
+	std::atomic<bool> _closed;
+	std::atomic<bool> _closing;
+
+	bool _isWritingAsync;
 };
--- JunCore/src/server/worldserver/main.cpp
+++ JunCore/src/server/worldserver/main.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
-#include <Networking/Socket.h>>
+#include <chrono>
+#include <ctime>
+#include <iomanip>
 
-struct MyStruct
+#include <Networking/Socket.h>
+
+using namespace std;
+
+void print_time();
+
+int main() {
+    print_time();
+    return 0;
+}
+
+void print_time()
 {
+    auto now = std::chrono::system_clock::now();
+    std::time_t currentTime = std::chrono::system_clock::to_time_t(now);
 
-};
+    std::tm* localTime = std::localtime(&currentTime);
 
-int main()
-{
-	Socket<MyStruct> socket;
-	std::cout << socket.f();
-	return 0;
+    std::cout << std::put_time(localTime, "%Y년 %m월 %d일 %H시 %M분 %S초") << std::endl;
 }
