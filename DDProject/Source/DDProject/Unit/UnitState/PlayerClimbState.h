74324d816b7c1001069a3cf9e9dd853d43adb83d
Jiwon <jae2021@gmail.com>
2024-10-04 17:37:51 +0900

    [최지원] 임시 커밋

---
+++ DDProject/Source/DDProject/Unit/UnitState/PlayerClimbState.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DDFundamental/Struct/DDStateBase.h"
+#include "PlayerClimbState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DDPROJECT_API UPlayerClimbState : public UDDStateBase
+{
+	GENERATED_BODY()
+
+	virtual void Begin() override;
+	virtual void Tick(float DeltaTime) override;
+	virtual void Exit() override;
+};
---
+++ DDProject/Source/DDProject/Unit/UnitState/PlayerIdleState.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerIdleState.h"
+
+void UPlayerIdleState::Begin()
+{
+	Super::Begin();
+}
+
+void UPlayerIdleState::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void UPlayerIdleState::Exit()
+{
+	Super::Exit();
+}
