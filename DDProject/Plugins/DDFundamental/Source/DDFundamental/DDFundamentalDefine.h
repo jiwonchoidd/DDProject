f919d2620725b0eb1b48317154856fd6029582fd
Geunwon Lee <dolees55@gmail.com>
2024-10-04 15:46:41 +0900

    [이근원] FundamentalDefine 추가 및 전역 매크로 추가

---
+++ DDProject/Plugins/DDFundamental/Source/DDFundamental/DDFundamentalDefine.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include "CoreMinimal.h"
+
+#define LOG_CALLINFO (FString(__FUNCTION__) + TEXT("(") + FString::FromInt(__LINE__) + TEXT(")"))
+
+#define	DD_Log(InFormat, ...)	  UE_LOG(DDLog, Log, InFormat, ##__VA_ARGS__);
+#define	DD_WARNING(InFormat, ...) UE_LOG(DDWarning, Warning, InFormat, ##__VA_ARGS__);
+#define	DD_ERROR(InFormat, ...)   UE_LOG(DDError, Error, InFormat, ##__VA_ARGS__);
+
+#define	DD_LOG_CALLINFO(InFormat, ...)	  UE_LOG(DDLog, Log, TEXT("%s %s"), *LOG_CALLINFO, *FString::Printf(InFormat, ##__VA_ARGS__));
+#define	DD_WARNING_CALLINFO(InFormat, ...) UE_LOG(DDWarning, Warning, TEXT("%s %s"), *LOG_CALLINFO, *FString::Printf(InFormat, ##__VA_ARGS__));
+#define	DD_ERROR_CALLINFO(InFormat, ...)   UE_LOG(DDError, Error, TEXT("%s %s"), *LOG_CALLINFO, *FString::Printf(InFormat, ##__VA_ARGS__));
+
+#define	DD_SCREEN(Format, ...)    if(GEngine) {GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT(Format), ##__VA_ARGS__));}
+
+#define DD_CHECK(expr) check(expr);
+#define DD_ENSURE(expr) ensure(expr);
+
+#define	DD_New new
+#define	DD_Delete delete
+
+#define	DD_NewObject NewObject
+#define	DD_DeleteObject(Object){					    \
+DD_CHECK(Object != nullptr);				\
+(Object->ConditionalBeginDestroy());		\
+};
