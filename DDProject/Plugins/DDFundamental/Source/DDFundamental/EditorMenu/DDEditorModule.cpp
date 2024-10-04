fb56f4e20edc8305cc9064d3dfa4c400df3a8be0
Geunwon Lee <dolees55@gmail.com>
2024-10-04 15:44:12 +0900

    [이근원] 모듈 인터페이스 추가 및 에디터 메뉴 툴바 추가

---
+++ DDProject/Plugins/DDFundamental/Source/DDFundamental/EditorMenu/DDEditorModule.cpp
@@ -0,0 +1,57 @@
+﻿#include "DDEditorModule.h"
+
+#include "LevelEditor.h"
+#include "DDFundamental/DDModuleInterface.h"
+
+#define LOCTEXT_NAMESPACE "DD_Editor"
+
+TSharedRef<FWorkspaceItem> FDDEditorModule::MenuRoot = FWorkspaceItem::NewGroup(FText::FromString("Menu Root"));
+
+IMPLEMENT_PRIMARY_GAME_MODULE(FDDEditorModule, DDProject, "DDProject");
+
+void FDDEditorModule::StartupModule()
+{
+	DDModuleInterface::StartupModule();
+
+	if (!IsRunningCommandlet())
+	{
+		FLevelEditorModule& levelEditorModule = FModuleManager::LoadModuleChecked<FLevelEditorModule>("LevelEditor");
+		ExtensibilityManager = levelEditorModule.GetMenuExtensibilityManager();
+		MenuExtender = MakeShareable(new FExtender);
+		MenuExtender->AddMenuBarExtension(TEXT("Window"), EExtensionHook::After, NULL, FMenuBarExtensionDelegate::CreateRaw(this, &FDDEditorModule::MakePulldownMenu));
+		ExtensibilityManager->AddExtender(MenuExtender);
+	}
+	
+	UE_LOG(LogTemp, Warning, TEXT("FDDEditorModule::StartupModule"));
+}
+
+void FDDEditorModule::ShutdownModule()
+{
+	DDModuleInterface::ShutdownModule();
+	
+}
+
+void FDDEditorModule::AddModuleListener()
+{
+	DDModuleInterface::AddModuleListener();
+}
+
+void FDDEditorModule::MakePulldownMenu(FMenuBarBuilder& menuBuilder)
+{
+	menuBuilder.AddPullDownMenu(
+		FText::FromString(TEXT("DD Studio")),
+		FText::FromString(TEXT("DD Stuido Editor")),
+		FNewMenuDelegate::CreateRaw(this, &FDDEditorModule::FillPulldownMenu),
+		TEXT("DD Studio"),
+		TEXT("DD Studio")
+	);
+}
+
+void FDDEditorModule::FillPulldownMenu(FMenuBuilder& menuBuilder)
+{
+	menuBuilder.BeginSection(TEXT(""), FText::FromName(EditorMenuNames::DD));
+	menuBuilder.AddMenuSeparator(EditorMenuNames::DD);
+	menuBuilder.EndSection();
+}
+
+#undef LOCTEXT_NAMESPACE
---
+++ DDProject/Plugins/DDFundamental/Source/DDFundamental/EditorMenu/DDToolTabBase.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "DDEditorModule.h"
+#include "DDFundamental/DDModuleInterface.h"
+
+class DDFUNDAMENTAL_API FDDToolTabBase : public IDDModuleListenerInterface, public TSharedFromThis<FDDToolTabBase>
+{
+	virtual ~FDDToolTabBase() override
+	{
+	}
+
+	virtual void OnStartupModule() override
+	{
+		Initialize();
+		FGlobalTabmanager::Get()->RegisterNomadTabSpawner(TabName, FOnSpawnTab::CreateRaw(this, &FDDToolTabBase::SpawnTab))
+			.SetGroup(FDDEditorModule::Get().GetMenuRoot())
+			.SetDisplayName(TabDisplayName)
+			.SetTooltipText(ToolTipText)
+			.SetIcon(FSlateIcon(FAppStyle::GetAppStyleSetName(), IconName));
+		
+	};
+
+	virtual void OnShutdownModule() override
+	{
+		FGlobalTabmanager::Get()->UnregisterNomadTabSpawner(TabName);
+	};
+
+	virtual void Initialize()
+	{
+	};
+
+	virtual TSharedRef<SDockTab> SpawnTab(const FSpawnTabArgs& _TabSpawnArgs)
+	{
+		return SNew(SDockTab);
+	};
+
+	virtual void MakeMenuEntry(FMenuBuilder& _MenuBuilder)
+	{
+		FGlobalTabmanager::Get()->PopulateTabSpawnerMenu(_MenuBuilder, TabName);
+	};
+	
+protected:
+	FName TabName;
+	FText TabDisplayName;
+	FText ToolTipText;
+	FName IconName;
+};
