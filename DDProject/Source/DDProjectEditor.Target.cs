11b7e8039f422c5b65099fd8234f3c05299609e3
Jiwon <jae2021@gmail.com>
2024-11-14 17:36:28 +0900

    [최지원] 5.5 버젼 업그레이드 누락된 부분 수정

--- DDProject/Source/DDProjectEditor.Target.cs
+++ DDProject/Source/DDProjectEditor.Target.cs
@@ -9,7 +9,7 @@ public class DDProjectEditorTarget : TargetRules
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
 		ExtraModuleNames.AddRange( new string[] { "DDProject", "DDProjectEditor" } );
 	}
 }
